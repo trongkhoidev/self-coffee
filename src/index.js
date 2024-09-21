6c11a584ec9c0d2a4f39332130da314b87fedd05
Trong Khoi <ntkdz28102005@gmai.com>
2024-09-21 21:30:31 +0700

    use library prettier, lint-staged, husky

--- src/index.js
+++ src/index.js
@@ -1,27 +1,28 @@
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
-const handlebars = require('express-handlebars'); 
+const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')))
-// HTTP logger
-app.use(morgan('combined'));
+const route = require('./routes');
+
+app.use(express.static(path.join(__dirname, 'public')));
+      app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+app.use(express.json());
 
 // Template engine
-app.engine('handlebars', handlebars.engine()); 
+      app.engine('handlebars', handlebars.engine());
 app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-  res.render('home');
-});
-
-app.get('/news', (req, res) => {
-  res.render('news');
-});
+// Routes init
+    route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`);
+    console.log(`Example app listening on port http://localhost:${port}`);
 });
