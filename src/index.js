0e9f5be08caa488cb045bbda1571835979db8794
Trong Khoi <ntkdz28102005@gmai.com>
2024-09-21 21:45:02 +0700

    nothinggggg

--- src/index.js
+++ src/index.js
@@ -9,14 +9,14 @@ const route = require('./routes');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
-  express.urlencoded({
-    extended: true,
-  }),
+    express.urlencoded({
+        extended: true,
+    }),
 );
 app.use(express.json());
 
 // Template engine
-    app.engine('handlebars', handlebars.engine());
+app.engine('handlebars', handlebars.engine());
 app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, 'resources/views'));
 
@@ -24,5 +24,5 @@ app.set('views', path.join(__dirname, 'resources/views'));
 route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`);
+    console.log(`Example app listening on port http://localhost:${port}`);
 });
